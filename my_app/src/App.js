0d68f2ccf0cbc42a766e0ff51ec7b607baaa5e52
Berk ipekoglu <berkipekoglu@gmail.com>
2022-06-29 11:46:39 +0300

    components props

---
+++ my_app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HelloWorld from './components/HelloWorld'
+
+function App() {
+  return (
+    <div>
+      Merhaba
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ my_app/src/components/HelloWorld.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './HelloWorld.css';
+
+const HelloWorld = () => {
+  return <div>Hellow World Componentinden, Selam!</div>;
+};
+
+export default HelloWorld;
